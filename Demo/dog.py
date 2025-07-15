5738612e082185ae4656bd96ca68e8d666d44b22
Shunelae <shunelaelaekhaing@gmail.com>
2025-07-15 09:30:56 +0630

    #inheritance

---
+++ Demo/dog.py
@@ -0,0 +1,26 @@
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self,name,age,breed):
+        self.name = name
+        self.age = age
+
+    def __str(self):
+        return f"{self.name} is {self.age} years old"
+    
+    def speak(self,sound):
+        return f"{self.name} says {sound}"
+    
+class JackRussellTerrier(Dog):
+    pass
+
+class Dachshund(Dog):
+    pass
+
+class Bulldog(Dog):
+    pass
+
+miles = JackRussellTerrier("Miles", 4)
+buddy = Dachshund("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
